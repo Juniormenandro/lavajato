e7d518fdfc320cf4d1a90cd6528649c7a1979078
juniorwa <100045666+juniorwa@users.noreply.github.com>
2023-08-05 22:36:54 +0100

    teste

--- src/pages/api/clientesServicos/[id].ts
+++ src/pages/api/clientesServicos/[id].ts
@@ -3,7 +3,7 @@ import { GetResult } from "@prisma/client/runtime/library";
 
 const prisma = new PrismaClient();
 
-export default async function handler(req: { method: string; query: { id: any; }; }, res: { json: (arg0: GetResult<{ id: string; carro: string; concluido: boolean; aguardandoPagamento: boolean; data: Date; selectedPayment: string; selectedProductId: string; selectedProductNane: string; selectedProdutPrice: string; selectedModel: string; selectedColor: string; selectedTime: string; selectedProductDefaultPrice: string; rawPrice: string; createdAt: Date; updatedAt: Date; clienteId: string; }, never> & {}) => void; status: (arg0: number) => { (): any; new(): any; json: { (arg0: { error: string; }): void; new(): any; }; }; }) {
+export default async function handler(req: { method: string; query: { id: any; }; }, res: { json: (arg0: GetResult<{ id: string; carro: string; concluido: boolean; aguardandoPagamento: boolean; data: Date; selectedPayment: string; selectedProductId: string; selectedProductNane: string; selectedProdutPrice: string; selectedModel: string; selectedColor: string; selectedTime: string; selectedProductDefaultPrice: string; rawPrice: string; clienteId: string; }, never> & {}) => void; status: (arg0: number) => { (): any; new(): any; json: { (arg0: { error: string; }): void; new(): any; }; }; }) {
   if (req.method === 'PUT') {
     const id = req.query.id;
     
