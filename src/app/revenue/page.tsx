878bcd0858838dd491c6013010edfc092550ad9e
juniorwa <100045666+juniorwa@users.noreply.github.com>
2023-09-06 20:01:11 +0100

    estilo revenue

--- src/app/revenue/page.tsx
+++ src/app/revenue/page.tsx
@@ -114,61 +114,59 @@ function App() {
 
 
   return (
-    <>
-    
-    <Header />
-    <div className='bg-gray-800  text-center'>
-      <div className='bg-gray-700 m-2 pb-10 pt-5 '>
-        <div>
-            <h1 className= ' bg-gray-900 m-3 pt-5 pb-5 text-2xl rounded-xl'>Selecione o Intervalo de Datas</h1>
-            <div className='pb-2 text-center'>
-                <label >
-                    <h2 className='text-white mb-2'>Data de início:</h2>
-
-                    <DatePicker className=' w-36 rounded-xl p-1' selected={startDate} onChange={(date) => setStartDate(date || new Date())} dateFormat="yyyy-MM-dd" />
-                </label>
-            </div>
-            <div>
-                <label>
-                    <h2 className='text-white mb-2 mt-5 '>Data de término:</h2>
-
-                    <DatePicker className=' w-36 rounded-xl p-1' selected={endDate} onChange={(date) => setEndDate(date || new Date())} dateFormat="yyyy-MM-dd" />
-                </label>
-            </div>
-            <Button className=' w-48 mt-10 ' onClick={getRevenueData} type={'button'} isLoading={false}>Obter Dados de Receita</Button>
-        </div>
-        
-        <div className='m-3'>
+  <>
+  <Header />
+    <div className=' text-center ' >
+      <div className='m-5'>
           <h3 className='bg-gray-900 text-2xl rounded-t-xl pb-1'>Receita</h3>
           <table border={1}>
-            
             <tbody>
               {revenueData?.monthlyRevenue.map((item, index) => (
-                <tr key={index}>
-                  <td className='bg-gray-800 m-1 text-white w-60 pb-2'>{item.selectedProductNane}</td>
-                  <td className='bg-gray-800 m-1 text-white w-40'>{item.selectedPayment}</td>
-                  <td className='bg-gray-800 m-1 text-white w-40'>{item._sum.rawPrice}</td>
-                </tr>
+              <tr key={index}>
+                <td className='bg-gray-800 m-1 text-white w-60 pb-2'>{item.selectedProductNane}</td>
+                <td className='bg-gray-800 m-1 text-white w-40'>{item.selectedPayment}</td>
+                <td className='bg-gray-800 m-1 text-white w-40'>{item._sum.rawPrice}</td>
+              </tr>
               ))}
             </tbody>
           </table>
           <p className=' bg-gray-800 pb-3 rounded-b-xl'></p>
         </div>
-        <div className='m-3'>
-        
-        <div>
+        <div className='m-5'>
+          <div>
             <h4 className='bg-gray-900 text-2xl rounded-t-xl pb-1'>Receita Total</h4>
             {revenueData?.totalMonthlyRevenue.map((item, index) => (
             <p className='bg-gray-800 pb-2  text-white' key={index}>{item.selectedPayment}: {item._sum.rawPrice}</p>
             ))}
             <p className=' bg-gray-800 pb-3 rounded-b-xl'></p>
+          </div>
         </div>
-       </div>
+      <div className='bg-gray-800 m-5 rounded-xl ' >
+        <h1 className= ' bg-gray-900 text-2xl rounded-t-xl pb-1'>Selecione o Intervalo de Datas</h1>
+        <div className='flex justify-center'>
+          <label className='mt-2 ml-10 mr-5 flex-1 flex justify-center' >
+            <div>
+              <h2 className='text-white'>Data de início:</h2>
+              <DatePicker className=' w-36 rounded-xl p-1' selected={startDate} onChange={(date) => setStartDate(date || new Date())} dateFormat="yyyy-MM-dd" />
+            </div>
+          </label>
+          <label className='mt-2 ml-5 mr-5 flex-1 flex justify-center'>
+            <div>
+              <h2 className='text-white'>Data de término:</h2>
+              <DatePicker className=' w-36 rounded-xl p-1' selected={endDate} onChange={(date) => setEndDate(date || new Date())} dateFormat="yyyy-MM-dd" />
+            </div>
+          </label>
+        </div>
+        <div className='p-5'>
+          <Button onClick={getRevenueData} type={'button'} isLoading={false}>Obter Dados de Receita</Button>
+        </div>
       </div>
     </div>
- 
-    </>
-   );
+    <div className='h-96'></div>
+    <div className='h-96'></div>
+  </>
+  
+  );
 }
 
 export default App;
